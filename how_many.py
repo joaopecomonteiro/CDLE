a80892e23f4e256994f82ebc0837a436c2b41ba3
Joao Monteiro <joaopecomonteiro@gmail.com>
2024-03-01 23:53:39 +0000

    d

---
+++ how_many.py
@@ -0,0 +1,8 @@
+def how_many_within_range2(i, row, minimum, maximum):
+   """Returns how many numbers lie within `maximum` and `minimum` in a given `row`"""
+   count = 0
+   for num in row:
+      if minimum <= num <= maximum:
+         count = count + 1
+ #  print(str(i) + ' ' + count)
+   return (i, count)
